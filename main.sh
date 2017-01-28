653eb6155a2d5d100d82c8228dab8140fe1da066
vintagesucks <nikolas@vintagesucks.de>
2017-01-28 04:18:00 +0100

    Double quote to prevent globbing and word splitting.
    https://github.com/koalaman/shellcheck/wiki

--- main.sh
+++ main.sh
@@ -429,7 +429,7 @@ function install_wordpress()
     wpadminpass=`dd if=/dev/urandom bs=1 count=32 2>/dev/null | base64 -w 0 | rev | cut -b 2- | rev | tr -dc 'a-zA-Z0-9'`
 
     # Protocol
-    if valid_ip ${domain}; then
+    if valid_ip "${domain}"; then
       protocol="http://"
     else
       protocol="https://"
