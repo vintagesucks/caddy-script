0ed7b50f2339270a7ece82b42becb050edc9ac82
Nikolas Evers <vintagesucks@users.noreply.github.com>
2019-09-11 11:13:23 +0200

    Use Node.js 12
    Active LTS Start 2019-10-22

--- main.sh
+++ main.sh
@@ -642,7 +642,7 @@ function tests()
   if [[ $TRAVIS_CI == 1 ]]; then
     echo "Testing installation"
     echo "Installing Node.js"
-    curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+    curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
     apt-get update
     sudo apt-get install -y nodejs
     echo "Installing Nightwatch"
