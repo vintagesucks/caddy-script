3839e39ca35b1f8fdd69fd1250a36c2e8ed197d2
Nikolas Evers <vintagesucks@users.noreply.github.com>
2020-02-20 17:21:08 +0100

    v2 draft init

--- tests/nightwatch/phpMyAdmin.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  'Title' : function (client) {
-    client
-      .url('http://127.0.0.1:1337/phpmyadmin/')
-      .waitForElementVisible('body', 1000)
-      .assert.title('phpMyAdmin')
-      .end();
-  }
-};
